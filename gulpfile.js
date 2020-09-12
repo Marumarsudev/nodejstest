91aa57f4df36b7ba2229ea7245e907554b39351e
Markus Peräkylä <markus.perakyla@seepiagames.com>
2020-09-12 17:52:41 +0300

    Did the tehtava kekw

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
+const spawn = require("child_process").spawn;
+const clean = require("gulp-clean");
+const minify = require("gulp-minify");
+let node;
 
+gulp.task("clean", function () {
+    return gulp.src("serve", {read: false})
+        .pipe(clean());
+});
 
 gulp.task("build", () => {
     const tsProject = ts.createProject("tsconfig.json");
@@ -11,12 +19,29 @@ gulp.task("build", () => {
     .pipe(gulp.dest("serve"));
 });
 
-gulp.task("serve", () => {
-    return run("node serve/server.js").exec();
+gulp.task('moveHtml', () => {
+  return gulp.src("./src/public/*.html")
+      .pipe(gulp.dest("serve/public"));
 });
 
-gulp.task("default", gulp.series(
-    "build",
-    "serve"
-))
-  exports.default = "default";
+gulp.task('serve', () => {
+    if (node) { console.log("Killing node!"); node.kill(); }
+    node = spawn('node', ['serve/server.js'], {stdio: 'inherit'})
+    node.on('close', function (code) {
+      if (code === 8) {
+        gulp.log('Error detected, waiting for changes...');
+      }
+    });
+});
+
+gulp.task("default", gulp.series("clean", "build", "moveHtml", "serve" ));
+
+exports.default = () => {
+  const run = gulp.series("clean", "build", "moveHtml", "serve" );
+  run();
+  gulp.watch("src/**", done => { run(); done();});
+};
+
+process.on('exit', function() {
+    if (node) node.kill()
+});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
 import express = require("express");
+import path = require("path");
+import fs = require("fs");
 import { area } from "./circle";
+
+const port: number = 3000;
 
 // Create a new express app instance
 const app: express.Application = express();
-app.get("/", function (req, res) {
-    res.send(area(29).toString());
+
+app.get("*", (req, res) => {
+    console.log(req.path);
+    console.log(path.extname(req.path));
+    if (path.extname(req.path) !== ".html") {
+        res.status(404).send("<title>404!!!</title>OwO!!! Sowwy wequested page not found!");
+    } else {
+        const newPath = path.join(__dirname + "/public" + req.path);
+        console.log(newPath);
+        fs.exists(newPath, (exists) => {
+            if (exists) {
+                res.sendFile(newPath);
+            } else {
+                res.status(404).send("<title>404!!!</title>OwO!!! Sowwy wequested page not found!");
+            }
+        });
+    }
 });
-app.listen(3000, function () {
-    console.log("App is listening on port 3000!");
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port.toString()}!`);
 });
